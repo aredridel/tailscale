2d333096ab1a06bab4465758364d36205913f3a4
Simon Law <sfllaw@tailscale.com>
2026-05-05 18:53:44 -0700

    cmd/tailscale/cli: add `tailscale exit-node suggest --force-probe`
    Add a new `--force-probe` flag to `tailscale exit-node suggest` that
    waits for a routecheck.Refresh to finish before suggesting an exit
    node.
    
    This flag is currently hidden from the help text, but this flag is a
    hint to the user that exit-node suggestions are based on routecheck
    reachability reports.
    
    Updates #17366
    Updates tailscale/corp#33033
    
    Signed-off-by: Simon Law <sfllaw@tailscale.com>

--- ipn/localapi/routecheck_disabled.go
+++ ipn/localapi/routecheck_disabled.go
@@ -6,11 +6,21 @@
 package localapi
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"tailscale.com/feature"
 )
 
 func (h *Handler) serveRouteCheck(w http.ResponseWriter, r *http.Request) {
 	panic(feature.ErrUnavailable.Error())
+}
+
+func (h *Handler) routeCheckProbe(ctx context.Context, timeout time.Duration) error {
+	return nil
+}
+
+func (h *Handler) routeCheckRefresh(ctx context.Context, timeout time.Duration) error {
+	return nil
 }
